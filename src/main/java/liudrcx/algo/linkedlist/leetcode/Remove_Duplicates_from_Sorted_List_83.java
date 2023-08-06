f8ea11b8060ce55474f55f1eb5278cdb78658d75
Allen Liu <liudrcx@foxmail.com>
2023-08-06 15:37:37 +0800

    leetcode linkedlist

---
+++ src/main/java/liudrcx/algo/linkedlist/leetcode/Remove_Duplicates_from_Sorted_List_83.java
@@ -0,0 +1,52 @@
+package liudrcx.algo.linkedlist.leetcode;
+
+/**
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+ */
+public class Remove_Duplicates_from_Sorted_List_83 {
+
+  /**
+   * Definition for singly-linked list.
+   * public class ListNode {
+   *     int val;
+   *     ListNode next;
+   *     ListNode() {}
+   *     ListNode(int val) { this.val = val; }
+   *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+   * }
+   */
+  class Solution1 {
+    public ListNode deleteDuplicates(ListNode head) {
+      if (head == null || head.next == null) {
+        return head;
+      }
+
+      if (head.val == head.next.val) {
+        return deleteDuplicates(head.next);
+      } else {
+        head.next = deleteDuplicates(head.next);
+        return head;
+      }
+    }
+  }
+
+  class Solution2 {
+    public ListNode deleteDuplicates(ListNode head) {
+      if (head == null || head.next == null) {
+        return head;
+      }
+
+      ListNode p1 = head, p2 = p1.next;
+      while(p2 != null) {
+        if (p1.val == p2.val) {
+          p1.next = p2.next;
+        } else {
+          p1 = p1.next;
+        }
+        p2 = p2.next;
+      }
+
+      return head;
+    }
+  }
+}
---
+++ src/main/java/liudrcx/algo/linkedlist/leetcode/Remove_Duplicates_from_Sorted_List_II_82.java
@@ -0,0 +1,57 @@
+package liudrcx.algo.linkedlist.leetcode;
+
+/**
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
+ */
+public class Remove_Duplicates_from_Sorted_List_II_82 {
+
+  /**
+   * Definition for singly-linked list.
+   * public class ListNode {
+   *     int val;
+   *     ListNode next;
+   *     ListNode() {}
+   *     ListNode(int val) { this.val = val; }
+   *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+   * }
+   */
+  class Solution1 {
+    public ListNode deleteDuplicates(ListNode head) {
+      if (head == null || head.next == null) {
+        return head;
+      }
+
+      if (head.val == head.next.val) {
+        ListNode p = head.next;
+        while(p != null && p.val == head.val) {
+          p = p.next;
+        }
+        return deleteDuplicates(p);
+      } else {
+        head.next = deleteDuplicates(head.next);
+        return head;
+      }
+    }
+  }
+
+  class Solution2 {
+    public ListNode deleteDuplicates(ListNode head) {
+      if (head == null || head.next == null) {
+        return head;
+      }
+
+      ListNode dummy = new ListNode(-1, head);
+      ListNode p1 = dummy, p2, p3;
+      while((p2 = p1.next) != null && (p3 = p2.next) != null) {
+        if (p2.val == p3.val) {
+          while((p3 = p3.next) != null && p3.val == p2.val) {}
+          p1.next = p3;
+        } else {
+          p1 = p1.next;
+        }
+      }
+
+      return dummy.next;
+    }
+  }
+}
