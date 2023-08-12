d86257a76599ade1a33210a3fb16c5c4780be921
Allen Liu <liudrcx@foxmail.com>
2023-08-12 11:20:20 +0800

    leetcode issue

---
+++ src/main/java/liudrcx/algo/linkedlist/leetcode/Palindrome_Linked_List_234.java
@@ -0,0 +1,99 @@
+package liudrcx.algo.linkedlist.leetcode;
+
+import javax.swing.*;
+
+/**
+ * https://leetcode.com/problems/palindrome-linked-list/
+ */
+public class Palindrome_Linked_List_234 {
+
+  /**
+   * Definition for singly-linked list.
+   * public class ListNode {
+   *     int val;
+   *     ListNode next;
+   *     ListNode() {}
+   *     ListNode(int val) { this.val = val; }
+   *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+   * }
+   */
+  class Solution1 {
+    //1 2 3 2 1
+    public boolean isPalindrome(ListNode head) {
+      if (head == null && head.next == null) {
+        return true;
+      }
+
+      ListNode middle = findMiddle(head);
+      ListNode reversed = reverse(middle);
+
+      ListNode p1 = head, p2 = reversed;
+      while(p2 != null) {
+        if (p1.val != p2.val) {
+          return false;
+        }
+        p1 = p1.next;
+        p2 = p2.next;
+      }
+      return true;
+    }
+
+    private ListNode findMiddle(ListNode head) {
+      ListNode slow = head, fast = head;
+      while(fast != null && fast.next != null) {
+        fast = fast.next.next;
+        slow = slow.next;
+      }
+      return slow;
+    }
+
+    private ListNode reverse(ListNode head) {
+      ListNode pre = null, curr = head;
+      while(curr != null) {
+        ListNode next = curr.next;
+        curr.next = pre;
+
+        pre = curr;
+        curr = next;
+      }
+      return pre;
+    }
+  }
+
+  class Solution2 {
+
+    public boolean isPalindrome(ListNode head) {
+      if (head == null || head.next == null) {
+        return true;
+      }
+
+      ListNode p1 = head, p2 = head;
+      ListNode n1 = null, o1 = head;
+      while(p2 != null && p2.next != null) {
+        p1 = p1.next;
+        p2 = p2.next.next;
+
+        //reverse linked list
+        o1.next = n1;
+        n1 = o1;
+        o1 = p1;
+      }
+
+      if (p2 != null) {
+        p1 = p1.next;
+      }
+
+      while(n1 != null) {
+        if (n1.val != p1.val) {
+          return false;
+        }
+
+        p1 = p1.next;
+        n1 = n1.next;
+      }
+
+      return true;
+    }
+
+  }
+}
