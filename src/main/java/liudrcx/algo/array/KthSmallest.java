9041acbc011a5b6b3a5dd10f86d7d9cbc9754b79
Allen Liu <liudrcx@foxmail.com>
2023-09-08 14:15:10 +0800

    KthSmallest

---
+++ src/main/java/liudrcx/algo/array/KthSmallest.java
@@ -0,0 +1,42 @@
+package liudrcx.algo.array;
+public class KthSmallest {
+
+  public static int kthSmallest(int[] arr, int k) {
+    if (arr == null || arr.length < k) {
+      return -1;
+    }
+
+    int p = partition(arr, 0, arr.length - 1);
+    while(p + 1 != k) {
+      if (p + 1 < k) {
+        p = partition(arr, p + 1, arr.length -1);
+      } else {
+        p = partition(arr, 0, p - 1);
+      }
+    }
+    return arr[p];
+  }
+
+  private static int partition(int[] arr, int p, int r) {
+    int pivot = arr[r];
+    int i = p;
+    for (int j = p; j < r; j++) {
+      if (arr[j] <= pivot) {
+        swap(arr, i, j);
+        i++;
+      }
+    }
+    swap(arr, i, r);
+    return i;
+  }
+
+  private static void swap(int[] arr, int i, int j) {
+    if (i == j) {
+      return;
+    }
+
+    int t = arr[i];
+    arr[i] = arr[j];
+    arr[j] = t;
+  }
+}
---
+++ src/test/java/liudrcx/algo/array/KthSmallestTest.java
@@ -0,0 +1,15 @@
+package liudrcx.algo.array;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class KthSmallestTest {
+
+  @Test
+  public void testKthSmallest() {
+    int[] arr = {3, 1, 4, 2, 5};
+    int result = KthSmallest.kthSmallest(arr, 3);
+    assertThat(result).isEqualTo(3);
+  }
+}
