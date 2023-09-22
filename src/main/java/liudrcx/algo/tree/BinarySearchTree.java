ed937b2aa8d5e014dcfdbf3f67a2287f056fc714
Allen Liu <liudrcx@foxmail.com>
2023-09-22 15:22:42 +0800

    BST get min max put

--- src/main/java/liudrcx/algo/tree/BinarySearchTree.java
+++ src/main/java/liudrcx/algo/tree/BinarySearchTree.java
@@ -2,4 +2,101 @@ package liudrcx.algo.tree;
 
 public class BinarySearchTree {
 
+  BSTNode root;
+
+  public Object get(int key) {
+    BSTNode node = root;
+    while(node != null) {
+      if (key < node.key) {
+        node = node.left;
+      } else if (node.key < key) {
+        node = node.right;
+      } else {
+        return node.value;
+      }
+    }
+    return null;
+  }
+
+  public Object min() {
+    BSTNode node = root;
+    while(node != null && node.left != null) {
+      node = node.left;
+    }
+    return node.value;
+  }
+
+  public Object max() {
+    BSTNode node = root;
+    while(node != null && node.right != null) {
+      node = node.right;
+    }
+    return node.value;
+  }
+
+  public void put(int key, Object value) {
+    BSTNode parent = null;
+    BSTNode node = root;
+
+    while(node != null) {
+      parent = node;
+      if (key < node.key) {
+        node = node.left;
+      } else if (node.key < key) {
+        node = node.right;
+      } else {
+        node.value = value;
+        return;
+      }
+    }
+
+    if (parent == null) {
+      root = new BSTNode(key, value);
+
+    } else if (key < parent.key) {
+      parent.left = new BSTNode(key, value);
+    } else {
+      parent.right = new BSTNode(key, value);
+    }
+  }
+
+  public boolean isSameTree(BinarySearchTree target) {
+    return isSameTree(root, target.root);
+  }
+
+  private boolean isSameTree(BSTNode left, BSTNode right) {
+    if (left == null && right == null) {
+      return true;
+    }
+
+    if (left == null || right == null) {
+      return false;
+    }
+
+    if (left.key != right.key) {
+      return false;
+    }
+
+    return isSameTree(left.left, right.left) && isSameTree(left.right, right.right);
+  }
+
+
+  public static class BSTNode {
+    private int key;
+    private Object value;
+    private BSTNode left;
+    private BSTNode right;
+
+    public BSTNode(int key, Object value) {
+      this.key = key;
+      this.value = value;
+    }
+
+    public BSTNode(int key, Object value, BSTNode left, BSTNode right) {
+      this.key = key;
+      this.value = value;
+      this.left = left;
+      this.right = right;
+    }
+  }
 }
--- src/test/java/liudrcx/algo/tree/BinarySearchTreeTest.java
+++ src/test/java/liudrcx/algo/tree/BinarySearchTreeTest.java
@@ -4,8 +4,65 @@ import liudrcx.algo.tree.BinarySearchTree.BSTNode;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static liudrcx.algo.tree.BinarySearchTree.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BinarySearchTreeTest {
 
+  @Test
+  public void testGet() {
+    BinarySearchTree tree = createTree();
+    assertEquals("张无忌", tree.get(1));
+    assertEquals("周芷若", tree.get(2));
+    assertEquals("宋青书", tree.get(3));
+    assertEquals("小昭", tree.get(4));
+    assertEquals("说不得", tree.get(5));
+    assertEquals("赵敏", tree.get(6));
+    assertEquals("殷离", tree.get(7));
+    assertNull(tree.get(8));
+  }
+
+  @Test
+  public void testMinMax() {
+    BinarySearchTree tree = createTree();
+    assertEquals("张无忌", tree.min());
+    assertEquals("殷离", tree.max());
+  }
+
+  @Test
+  public void testPut() {
+    BinarySearchTree tree = new BinarySearchTree();
+    tree.put(4, new Object());
+    tree.put(2, new Object());
+    tree.put(6, new Object());
+    tree.put(1, new Object());
+    tree.put(3, new Object());
+    tree.put(7, new Object());
+    tree.put(5, new Object());
+    assertTrue(tree.isSameTree(createTree()));
+    tree.put(1, "教主张无忌");
+    assertEquals("教主张无忌", tree.get(1));
+  }
+
+  public BinarySearchTree createTree() {
+        /*
+                     4
+                   /   \
+                  2     6
+                 / \   / \
+                1   3 5   7
+         */
+    BSTNode n1 = new BSTNode(1, "张无忌");
+    BSTNode n3 = new BSTNode(3, "宋青书");
+    BSTNode n2 = new BSTNode(2, "周芷若", n1, n3);
+
+    BSTNode n5 = new BSTNode(5, "说不得");
+    BSTNode n7 = new BSTNode(7, "殷离");
+    BSTNode n6 = new BSTNode(6, "赵敏", n5, n7);
+    BSTNode root = new BSTNode(4, "小昭", n2, n6);
+
+    BinarySearchTree tree = new BinarySearchTree();
+    tree.root = root;
+    return tree;
+  }
 }
