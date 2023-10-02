477edacada3bf5fcd637fcb0be82d9b53fb9b278
Allen Liu <liudrcx@foxmail.com>
2023-10-02 10:45:58 +0800

    leetcode1

---
+++ src/main/java/liudrcx/algo/hashmap/leetcode/Two_Sum_1.java
@@ -0,0 +1,26 @@
+package liudrcx.algo.hashmap.leetcode;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * https://leetcode.com/problems/two-sum/description/
+ */
+public class Two_Sum_1 {
+
+  class Solution {
+    public int[] twoSum(int[] nums, int target) {
+      Map<Integer, Integer> map = new HashMap<>();
+      for (int i = 0; i < nums.length; i++) {
+        if (map.containsKey(target - nums[i])) {
+          return new int[] {map.get(target - nums[i]), i};
+        } else {
+          map.put(nums[i], i);
+        }
+      }
+      return new int[] {-1, -1};
+    }
+  }
+}
