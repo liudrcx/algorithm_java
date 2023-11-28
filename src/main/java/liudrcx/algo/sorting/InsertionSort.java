4c6fae1a82fcd03a0695df53dd993f807ff6f14f
Allen Liu <liudrcx@foxmail.com>
2023-11-28 18:52:06 +0800

    insert sorting

--- src/main/java/liudrcx/algo/sorting/InsertionSort.java
+++ src/main/java/liudrcx/algo/sorting/InsertionSort.java
@@ -19,4 +19,24 @@ public class InsertionSort {
       arr[j + 1] = value;
     }
   }
+
+  public void sort_recursion(int[] arr) {
+    sort_recursion(arr, 1);
+  }
+
+  private void sort_recursion(int[] arr, int low) {
+    if (low == arr.length) {
+      return;
+    }
+
+    int t = arr[low];
+    int i = low - 1;
+    while(i >= 0 && arr[i] > t) {
+      arr[i + 1] = arr[i];
+      i--;
+    }
+
+    arr[i + 1] = t;
+    sort_recursion(arr, low + 1);
+  }
 }
