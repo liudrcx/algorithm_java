4bfdf26dd69e149d4bbef7c7564e0ed60b04f4ab
Allen Liu <liudrcx@foxmail.com>
2024-05-11 12:13:15 +0800

    code change

--- src/main/java/liudrcx/problems/array/Merge_Sorted_Array_leetcode_88.java
+++ src/main/java/liudrcx/problems/array/Merge_Sorted_Array_leetcode_88.java
@@ -1,4 +1,4 @@
-package liudrcx.algo.array.issue;
+package liudrcx.ds.list.array;
 
 /**
  * https://leetcode.com/problems/merge-sorted-array/description/
@@ -10,7 +10,7 @@ package liudrcx.algo.array.issue;
  * The final sorted array should not be returned by the function, but instead be stored inside the array nums1.
  * To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
  */
-public class Merge_Sorted_Array_88 {
+public class Merge_Sorted_Array_leetcode_88 {
 
   class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
--- src/test/java/liudrcx/ds/list/array/DynamicArrayTest.java
+++ src/test/java/liudrcx/ds/list/array/DynamicArrayTest.java
@@ -1,5 +1,6 @@
 package liudrcx.algo.array;
 
+import liudrcx.ds.list.array.DynamicArray;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
