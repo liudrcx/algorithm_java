72e6f36eab6e810c751323cdc5525fadc03d6041
Allen Liu <liudrcx@foxmail.com>
2024-07-12 22:28:51 +0800

    add case

---
+++ src/main/java/liudrcx/concurrency/TestPrint.java
@@ -0,0 +1,11 @@
+package liudrcx.concurrency;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j(topic = "TestPrint")
+public class TestPrint {
+
+  public static void main(String[] args) {
+    log.debug("hello");
+  }
+}
